5d0e8ed5487c5d5f18145a5846734fee71f21750
Michael Mayer <39913724+dwaltsch@users.noreply.github.com>
2023-10-17 11:20:40 +0200

    added Main

---
+++ Design Patterns/src/at/mayer/spblue/tech/MainGame.java
@@ -0,0 +1,27 @@
+package at.mayer.spblue.tech;
+
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class MainGame extends BasicGame {
+    public MainGame(String title) {
+        super(title);
+    }
+
+    @Override
+    public void init(GameContainer gc) throws SlickException {
+
+    }
+
+    @Override
+    public void update(GameContainer gc, int i) throws SlickException {
+
+    }
+
+    @Override
+    public void render(GameContainer gc, Graphics graphics) throws SlickException {
+
+    }
+}
