8259cb69dc6e73938b71efce3b814fd4f94ef15e
Michael Mayer <39913724+dwaltsch@users.noreply.github.com>
2023-10-18 15:16:05 +0200

    slick add actor

--- Design Patterns/src/at/mayer/spblue/tech/CircleActor.java
+++ Design Patterns/src/at/mayer/spblue/tech/CircleActor.java
@@ -5,8 +5,15 @@ import org.newdawn.slick.Graphics;
 
 public class CircleActor {
     private double x,y;
-    public void update(GameContainer gc,int delta){
 
+    public CircleActor(double x, double y) {
+        super();
+        this.x = x;
+        this.y = y;
+    }
+
+    public void update(GameContainer gc, int delta){
+        this.x++;
     }
     public void render(Graphics graphics){
         graphics.drawOval((float)this.x,(float)this.y,20,20);
--- Design Patterns/src/at/mayer/spblue/tech/MainGame.java
+++ Design Patterns/src/at/mayer/spblue/tech/MainGame.java
@@ -4,23 +4,28 @@ import org.newdawn.slick.*;
 import org.newdawn.slick.tests.AnimationTest;
 
 public class MainGame extends BasicGame {
-    private CircleActor cal;
+    private CircleActor cal,cal2;
     public MainGame(String title) {
         super(title);
     }
 
     @Override
+    public void render(GameContainer gc, Graphics graphics) throws SlickException {
+        this.cal.render(graphics);
+        this.cal2.render(graphics);
+    }
+    @Override
     public void init(GameContainer gc) throws SlickException {
         this.cal = new CircleActor(100,100);
+        this.cal2 = new CircleActor(100,100);
     }
 
     @Override
-    public void update(GameContainer gc, int i) throws SlickException {
+    public void update(GameContainer gc, int delta) throws SlickException {
+        this.cal.update(gc,delta);
+        this.cal2.update(gc,delta);
+    }
 
-    }
-    @Override
-    public void render(GameContainer gc, Graphics graphics) throws SlickException {
-    }
     public static void main(String[] argv) {
         try {
             AppGameContainer container = new AppGameContainer(new MainGame("test"));
