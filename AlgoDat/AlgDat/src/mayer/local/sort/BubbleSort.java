3aca8b7c35ee08e75f3436f795ad90f53e97ec1c
Michael Mayer <39913724+dwaltsch@users.noreply.github.com>
2023-12-05 12:20:36 +0100

    würds mi

--- AlgoDat/AlgDat/src/mayer/local/sort/BubbleSort.java
+++ AlgoDat/AlgDat/src/mayer/local/sort/BubbleSort.java
@@ -3,6 +3,15 @@ package mayer.local.sort;
 public class BubbleSort implements Algo{
     @Override
     public int[] doSort(int[] input) {
+        for (int y = 0; y < input.length; y++) {
+            for (int i = 0; i < input.length - 1; i++) {
+                if(input[i] > input[i+1]){
+                    int zwischenspeicher = input[i];
+                    input[i] = input[i + 1];
+                    input[i + 1] = zwischenspeicher;
+                }
+            }
+        }
         return input;
     }
 }
